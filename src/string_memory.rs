1ec2720e77f8d2b213e94b8641d3d729a33e28eb
Filippo Ranza <filipporanza@gmail.com>
2020-08-07 19:01:49 +0200

    automatically reformatted code

---
+++ src/string_memory.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct StringMemory {
+    buff: HashMap<usize, String>,
+    index: usize,
+}
+
+impl StringMemory {
+    pub fn new() -> Self {
+        Self {
+            buff: HashMap::new(),
+            index: 0,
+        }
+    }
+
+    pub fn insert_string(&mut self, s: String) -> usize {
+        let key = self.index;
+        self.index += 1;
+        self.buff.insert(key, s);
+        key
+    }
+
+    pub fn remove_strings(&mut self, s_vec: &Vec<usize>) {
+        for i in s_vec {
+            self.buff.remove(i);
+        }
+    }
+
+    pub fn remove_string(&mut self, index: usize) {
+        self.buff.remove(&index);
+    }
+
+    pub fn get_string(&self, index: usize) -> &str {
+        let tmp = self.buff.get(&index);
+        tmp.unwrap()
+    }
+}
