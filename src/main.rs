7b18a2a499a049de1d624ef1b1cc06b8f9792c32
Filippo Ranza <filipporanza@gmail.com>
2021-01-21 10:19:08 +0100

    Improve error messages

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,19 @@ struct CLIArguments {
     file: PathBuf,
 }
 
-fn compile_and_run(file: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
-    let (prog, prog_mem, str_mem) = program_load::load_program(file)?;
-    engine::run_program(prog, prog_mem, str_mem)?;
-    Ok(())
+
+fn compile_and_run(file: &PathBuf) -> Result<(), String> {
+    let res = program_load::load_program(file);
+    let (prog, prog_mem, str_mem) = match res {
+        Ok((prog, prog_mem, str_mem)) => (prog, prog_mem, str_mem),
+        Err(err) => return Err(format!("Error while loading {:?}\n{}", file, err))
+    };
+
+    let run_stat = engine::run_program(prog, prog_mem, str_mem);
+    match run_stat {
+        Ok(()) => Ok(()),
+        Err(err) => Err(format!("Error while running {:?}\n{}", file, err))
+    }
 }
 
 fn main() {
